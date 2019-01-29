c2e6cd3dea743963b4a55b632b38a124ba817c3e
WuLuyang <wuluyang0210@163.com>
2019-01-29 18:12:35 +0800

    核心功能基本完成

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const categoryRouter = require('./routes/admin/category');
 const adminRouter = require('./routes/admin/admin');
+const dishRouter = require('./routes/admin/dish');
 
 //启动主服务器
 var app = express();
@@ -25,3 +26,4 @@ app.use(bodyParser.json());//把JSON格式的请求主体数据解析出来放�
 //挂载路由器
 app.use('/admin/category',categoryRouter);
 app.use('/admin',adminRouter);
+app.use('/admin/dish',dishRouter);
